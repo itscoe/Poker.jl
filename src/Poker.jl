88c7586ca0710f35f6eee878beac5ae085ffcf2a
itscoe <itslagle@coe.edu>
2020-12-02 19:57:47 -0600

    removed Base from export that seemed to be causing issues

--- src/Poker.jl
+++ src/Poker.jl
@@ -4,8 +4,7 @@ include("cards.jl")
 include("odds.jl")
 include("game.jl")
 
-export Base.:+, Base.:-, deal!, cards, Base.:isless, isflush, to_string,
-    Base.:<, Base.:>, calculate_odds, get_preflop_odds, bet, get_return,
-    get_returns, get_returns_table
+export +, -, >, <, deal!, cards, isless, isflush, to_string, calculate_odds,
+    get_preflop_odds, bet, get_return, get_returns, get_returns_table
 
 end
