92afa3409ed0fcd6ed429608248fe5ef2a7338ef
itscoe <itslagle@coe.edu>
2020-12-02 20:23:39 -0600

    deal is exported rather than deal! (seemed to not make a difference)

--- src/Poker.jl
+++ src/Poker.jl
@@ -4,7 +4,7 @@ include("cards.jl")
 include("odds.jl")
 include("game.jl")
 
-export +, -, >, <, deal!, cards, isless, isflush, to_string, calculate_odds,
+export +, -, >, <, deal, cards, isless, isflush, to_string, calculate_odds,
     get_preflop_odds, bet, get_return, get_returns, get_returns_table
 
 end
